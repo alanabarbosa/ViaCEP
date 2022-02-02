89a9875ce280a22bb7b023c919208eec00f5cddb
alanabarbosa <itsalanayall@hotmail.com>
2022-02-02 15:02:27 -0300

    Altera

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Buscar CEP Via API - ViaCep  🔍📌
 
-Aplicação feita em Javascript, para consultar CEP de endereçamento do Brasil gratuitamente. Para Consultar CEP basta você fazer uma **requisição http** para a **API do ViaCep** e então obter o retorno com informações como **CEP**, nome da **Cidade**, **Código do Município**, **UF**, etc.
+Aplicação feita em Javascript, para consultar CEP de endereçamento do Brasil gratuitamente. Para Consultar CEP basta você fazer uma **requisição http** para a **API do ViaCep** e então obter o retorno com informações como **CEP**, nome da **Cidade**, **Código do Município**, **UF**, etc
 
 # Imagens 📸
 <img src="images/readme.png">
--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
     function initFetchCep() {
         const inputCep = document.getElementById('cep');
         const btnCep = document.getElementById('btnCep')
-        const resultadoCep = document.querySelector('.resultadoCep')
         const box = document.querySelectorAll('.info')
         const img = document.querySelector('.box_img')
         const img2 = document.querySelector('.box_img2')
@@ -10,7 +9,32 @@
 
         function handleClick(event) {
             event.preventDefault();
-            var cep = inputCep.value;
+
+            function buscaCep(cep) {
+                var cep = inputCep.value;
+                fetch(`https://viacep.com.br/ws/${cep}/json/`)
+                    .then(response => response.json())
+                    .then(body => {
+                        const nomeRua = document.getElementById('nomeRua')
+                        const nomeBairro = document.getElementById('nomeBairro')
+                        const complemento = document.getElementById('complemento')
+                        const gia = document.getElementById('gia')
+                        const ibge = document.getElementById('ibge')
+                        const localidade = document.getElementById('localidade')
+                        const uf = document.getElementById('uf')
+                        nomeRua.innerText = body.logradouro;
+                        nomeBairro.innerText = body.bairro;
+                        complemento.innerText = body.complemento;
+                        gia.innerText = body.gia;
+                        ibge.innerText = body.ibge;
+                        localidade.innerText = body.localidade;
+                        uf.innerText = body.uf;
+                        img2.style.display = "flex";
+                        img.style.display = "none";
+                    }).catch(e => {
+                        console.log('Não encontrado')
+                    })
+            }
             buscaCep(cep);
         }
 
@@ -22,30 +46,7 @@
         }
         isEmpty()
 
-        function buscaCep(cep) {
-            fetch(`https://viacep.com.br/ws/${cep}/json/`)
-                .then(response => response.json())
-                .then(body => {
-                    const nomeRua = document.getElementById('nomeRua')
-                    const nomeBairro = document.getElementById('nomeBairro')
-                    const complemento = document.getElementById('complemento')
-                    const gia = document.getElementById('gia')
-                    const ibge = document.getElementById('ibge')
-                    const localidade = document.getElementById('localidade')
-                    const uf = document.getElementById('uf')
-                    nomeRua.innerText = body.logradouro;
-                    nomeBairro.innerText = body.bairro;
-                    complemento.innerText = body.complemento;
-                    gia.innerText = body.gia;
-                    ibge.innerText = body.ibge;
-                    localidade.innerText = body.localidade;
-                    uf.innerText = body.uf;
-                    img2.style.display = "flex";
-                    img.style.display = "none";
-                }).catch(e => {
-                    console.log('Não encontrado')
-                })
-        }
+
     }
     initFetchCep()
 
