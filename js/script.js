66ae06d31a04de0aeebe1206ee294927e070925c
alanabarbosa <itsalanayall@hotmail.com>
2022-02-02 10:23:15 -0300

    Segundo commit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
-    function initFetchBitcoin() {
+    function initFetchCep() {
         const inputCep = document.getElementById('cep');
         const btnCep = document.getElementById('btnCep')
         const resultadoCep = document.querySelector('.resultadoCep')
         const box = document.querySelectorAll('.info')
         const img = document.querySelector('.box_img')
         const img2 = document.querySelector('.box_img2')
-        
+            // console.log();
+
         btnCep.addEventListener('click', handleClick);
+
         function handleClick(event) {
             event.preventDefault();
-            var cep = inputCep.value;    
+            var cep = inputCep.value;
             buscaCep(cep);
         }
+
         function isEmpty() {
-            if(box.lenght !== 0) {
+            if (Object.keys(box).length !== 0) {
                 img2.style.display = "none";
                 img.style.display = "flex";
             }
-        }isEmpty() 
-        
+        }
+        isEmpty()
+
+
+
+
         function buscaCep(cep) {
             fetch(`https://viacep.com.br/ws/${cep}/json/`)
                 .then(response => response.json())
@@ -29,7 +36,7 @@
                     const gia = document.getElementById('gia')
                     const ibge = document.getElementById('ibge')
                     const localidade = document.getElementById('localidade')
-                    const uf = document.getElementById('uf')    
+                    const uf = document.getElementById('uf')
                     nomeRua.innerText = body.logradouro;
                     nomeBairro.innerText = body.bairro;
                     complemento.innerText = body.complemento;
@@ -39,37 +46,39 @@
                     uf.innerText = body.uf;
                     img2.style.display = "flex";
                     img.style.display = "none";
+                }).catch(erro => {
+                    alert(erro)
                 })
         }
     }
-    initFetchBitcoin() 
+    initFetchBitcoin()
 
     function initModal() {
         const botaoAbrir = document.querySelector('[data-modal="abrir"]');
         const botaoFechar = document.querySelector('[data-modal="fechar"]');
         const containerModal = document.querySelector('[data-modal="container"]');
-    
+
         if (botaoAbrir && botaoFechar && containerModal) {
             function abrirModal(event) {
-                if(document.getElementById("cep").value == ""){
+                if (document.getElementById("cep").value == "") {
                     event.preventDefault();
                     containerModal.classList.add('ativo')
                 }
             }
-    
+
             function fecharModal(event) {
                 event.preventDefault();
                 containerModal.classList.remove('ativo')
             }
-    
+
             function cliqueFora(event) {
                 if (event.target == this)
                     fecharModal(event)
             }
-    
+
             botaoAbrir.addEventListener('click', abrirModal);
             botaoFechar.addEventListener('click', fecharModal);
             containerModal.addEventListener('click', cliqueFora);
         }
     }
-    initModal() 
+    initModal()
